99d552b8f0a6473a239dd494eed2129b0d24cf0f
unknown <mayasbey16@gmail.com>
2026-05-22 14:15:59 +0100

    final fix admin panel problem and justificatifs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -19,8 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     # Third-party
     'rest_framework',
