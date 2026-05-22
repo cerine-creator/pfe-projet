932b71ca1ef0a60cdc77a054fb59292a957d5df6
unknown <mayasbey16@gmail.com>
2026-05-22 11:58:01 +0100

    support justificatif using cloudinary

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -178,3 +178,10 @@ SIMPLE_JWT = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
